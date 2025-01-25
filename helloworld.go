7e3f8cd2d430b30d56e02eb6247221af9b6e58a6
satish <satishbabu2794@gmail.com>
2025-01-25 23:12:06 +0530

    secrets added

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	// Access the secret from the environment variable
+	secret := os.Getenv("MY_SECRET")
+	fmt.Printf("My secret is '%s'\n", secret)
 }
+
